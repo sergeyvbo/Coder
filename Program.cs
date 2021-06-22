2653302d38beb94fc6fa80c8d4478ed5c3609c1d
Dmitriy <ic2de8500@gmail.com>
2021-06-22 05:06:24 +0500

    новый робот + рандомный выбор файла

--- Program.cs
+++ Program.cs
@@ -1,27 +1,57 @@
-﻿using Desktop.Robot;
-using Desktop.Robot.Extensions;
-using System;
+﻿using System;
 using System.IO;
+using System.Threading;
+using WindowsInput;
+using WindowsInput.Native;
 
-string filename = GetRandomFile(@"C:\temp\");
-var random = new Random();
-const int MinDelay = 10;
-const int MaxDelay = 300;
-
-if (!File.Exists(filename))
-{
-    return;
-}
-Console.WriteLine(filename);
+InputSimulator inputSimulator = new();
+var files = GetAllFile(@"C:\temp\");
 Console.ReadLine();
-var robot = new Robot();
+Thread.Sleep(2000);
 
-foreach (char ch in File.ReadAllText(filename))
+foreach (var file in files)
 {
-    robot.Type(ch.ToString(),(uint)random.Next(MinDelay, MaxDelay));
+    ; string filename = file;
+    var random = new Random();
+    const int MinDelay = 10;
+    const int MaxDelay = 1000;
+
+    if (!File.Exists(filename))
+    {
+        return;
+    }
+
+
+    foreach (char ch in File.ReadAllText(filename))
+    {
+        Thread.Sleep(random.Next(MinDelay, MaxDelay));
+        inputSimulator.Keyboard.TextEntry(ch);
+
+    }
+    NewFile(filename);
+
 }
 
-string GetRandomFile(string v)
+
+string[] GetAllFile(string path)
 {
-    return @"C:\temp\AzureStorageService.cs";
+    var random = new Random();
+    var files = Directory.GetFiles(path, "*.cs", SearchOption.AllDirectories);
+    for (int i = 0; i < files.Length; i++)
+    {
+        var nf = random.Next(0, files.Length);
+        var temp = files[nf];
+        files[nf] = files[i];
+        files[i] = temp;
+    }
+    return files;
+}
+
+void NewFile(string file)
+{
+
+    inputSimulator.Keyboard.ModifiedKeyStroke(VirtualKeyCode.CONTROL, VirtualKeyCode.VK_A);
+    inputSimulator.Keyboard.KeyPress(VirtualKeyCode.BACK);
+    //---------------
+
 }
