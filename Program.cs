5851b8d6df83654ed28c2aea15a9fb0a5b06103d
Sergey B <sergeyvbo@outlook.com>
2021-06-23 22:58:40 +0300

    refactoring

--- Program.cs
+++ Program.cs
@@ -4,54 +4,67 @@ using System.Threading;
 using WindowsInput;
 using WindowsInput.Native;
 
-InputSimulator inputSimulator = new();
-var files = GetAllFile(@"C:\temp\");
-Console.ReadLine();
-Thread.Sleep(2000);
-
-foreach (var file in files)
+namespace Coder
 {
-    ; string filename = file;
-    var random = new Random();
-    const int MinDelay = 10;
-    const int MaxDelay = 1000;
-
-    if (!File.Exists(filename))
+    class Program
     {
-        return;
+        static InputSimulator inputSimulator = new();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press Enter to start");
+            Console.ReadLine();
+            Thread.Sleep(2000);
+            NewFile();
+            TypeText();
+        }
+
+        static void TypeText()
+        {
+            var files = GetAllFiles(@"C:\temp\");
+
+            foreach (var file in files)
+            {
+                ; string filename = file;
+                var random = new Random();
+                const int MinDelay = 10;
+                const int MaxDelay = 400;
+
+                if (!File.Exists(filename))
+                {
+                    return;
+                }
+
+                foreach (char ch in File.ReadAllText(filename))
+                {
+                    Thread.Sleep(random.Next(MinDelay, MaxDelay));
+                    inputSimulator.Keyboard.TextEntry(ch);
+
+                }
+                NewFile();
+            }
+        }
+
+        static string[] GetAllFiles(string path)
+        {
+            var random = new Random();
+            var files = Directory.GetFiles(path, "*.cs", SearchOption.AllDirectories);
+            for (int i = 0; i < files.Length; i++)
+            {
+                var nf = random.Next(0, files.Length);
+                var temp = files[nf];
+                files[nf] = files[i];
+                files[i] = temp;
+            }
+            return files;
+        }
+
+        static void NewFile()
+        {
+            inputSimulator.Keyboard.KeyDown(VirtualKeyCode.LCONTROL);
+            inputSimulator.Keyboard.KeyPress(VirtualKeyCode.VK_A);
+            inputSimulator.Keyboard.KeyUp(VirtualKeyCode.LCONTROL);
+            inputSimulator.Keyboard.KeyPress(VirtualKeyCode.BACK);
+        }
+        
     }
-
-
-    foreach (char ch in File.ReadAllText(filename))
-    {
-        Thread.Sleep(random.Next(MinDelay, MaxDelay));
-        inputSimulator.Keyboard.TextEntry(ch);
-
-    }
-    NewFile(filename);
-
-}
-
-
-string[] GetAllFile(string path)
-{
-    var random = new Random();
-    var files = Directory.GetFiles(path, "*.cs", SearchOption.AllDirectories);
-    for (int i = 0; i < files.Length; i++)
-    {
-        var nf = random.Next(0, files.Length);
-        var temp = files[nf];
-        files[nf] = files[i];
-        files[i] = temp;
-    }
-    return files;
-}
-
-void NewFile(string file)
-{
-
-    inputSimulator.Keyboard.ModifiedKeyStroke(VirtualKeyCode.CONTROL, VirtualKeyCode.VK_A);
-    inputSimulator.Keyboard.KeyPress(VirtualKeyCode.BACK);
-    //---------------
-
 }
