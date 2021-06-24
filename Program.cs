e9bba28b7a4d72d3fea6c2cd3a7d040ff9c6fca2
Sergey B <sergeyvbo@outlook.com>
2021-06-24 12:40:11 +0300

    набросок ScenarioManager

--- Program.cs
+++ Program.cs
@@ -11,11 +11,25 @@ namespace Coder
         static InputSimulator inputSimulator = new();
         static void Main(string[] args)
         {
+            //TODO чтение конфига
+            //TODO парсинг параметров командной строки и оверрайд конфига
+
+
             Console.WriteLine("Press Enter to start");
             Console.ReadLine();
             Thread.Sleep(2000);
             NewFile();
             TypeText();
+
+            //var scenarioManager = ScenarioManager.Create();
+
+            while(true)
+            {
+                //scenarioManager.GetNextScenario().Execute();
+            }
+
+            // ожидаемое максимальное время работы ~40 минут.
+            // выбирать не рандомные сценари, а вперемешку: большие и маленькие
 
             //сценарии
             //1. набор кода в VS
@@ -24,6 +38,8 @@ namespace Coder
             //4. просмотр видео
             //5. чтение metanit (как перейти на следующую страницу?)
             //6. переписка в linq
+            //7. просмотр почты
+            //8. редкие нажатия для поддержания соединения
 
         }
 
