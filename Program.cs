542f9cd9c5785a48de0cf57ee0fb173a1ee569de
Sergey B <sergeyvbo@outlook.com>
2021-07-01 22:16:55 +0300

    add configuration (json, args)

--- Program.cs
+++ Program.cs
@@ -1,24 +1,44 @@
 ﻿using Coder.Scenarios;
+using Microsoft.Extensions.Configuration;
 using System;
+using System.Linq;
 using System.Threading;
-using WindowsInput;
 
 namespace Coder
 {
     class Program
     {
-        static InputSimulator inputSimulator = new();
         static void Main(string[] args)
         {
-            //TODO чтение конфига
-            //TODO парсинг параметров командной строки и оверрайд конфига
 
+            if (args.Any(s => s.Contains("help") || s.Contains("?") || s.Contains("/h")
+            ))
+            {
+                ShowHelp();
+                Console.ReadLine();
+                return;
+            }
 
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddCommandLine(args)
+                .Build();
+            
+            var scenarioManager = new ScenarioManager(configuration);
+
+            if (args.Any(s => s == "/scenarios"))
+            {
+                Console.WriteLine("Scenarios:");
+                ShowScenarios(scenarioManager);
+                Console.ReadLine();
+                return;
+            }
+
+            Console.WriteLine($"Next scenario: {scenarioManager.GetNextScenarioName()}");
             Console.WriteLine("Press Enter to start");
+            Console.WriteLine("Press Ctrl-Brake to stop");
             Console.ReadLine();
             Thread.Sleep(2000);
-
-            var scenarioManager = new ScenarioManager();
 
             while (true)
             {
@@ -38,6 +58,23 @@ namespace Coder
             //7. просмотр почты
             //8. редкие нажатия для поддержания соединения
 
+        }
+
+        private static void ShowScenarios(ScenarioManager scenarioManager)
+        {
+            foreach (var s in scenarioManager.Scenarios)
+            {
+                Console.WriteLine(s);
+            }
+        }
+
+        private static void ShowHelp()
+        {
+            Console.WriteLine("Command-line arguments:");
+            Console.WriteLine("/help | /h | /? - show this message.");
+            Console.WriteLine("/scenario <scenarioName> - select scenario");
+            Console.WriteLine("/scenarios - get list of scenarios");
+            Console.WriteLine("Press any key to exit.");
         }
     }
 }
--- Scenarios/ScenarioManager.cs
+++ Scenarios/ScenarioManager.cs
@@ -1,10 +1,98 @@
-﻿namespace Coder.Scenarios
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+
+namespace Coder.Scenarios
 {
     public class ScenarioManager
     {
+        private readonly IConfiguration _configuration;
+        
+
+        public string[] Scenarios { get; set; }
+        public Queue<string> ScenarioQueue;
+
+        public ScenarioManager(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            Scenarios = InitScenarios();
+            ScenarioQueue = InitScenarioQueue();
+        }
+
+        private string[] InitScenarios()
+        {
+            var scenarios = new string[]
+            {
+                "CodingScenario",
+                "ScrollingScenario",
+                "StayOnlineScenario"
+            };
+
+            return scenarios;
+        }
+
+        private Queue<string> InitScenarioQueue()
+        {
+            var queue = new Queue<string>();
+            var confScenario = _configuration["scenario"];
+            if (confScenario is not null)
+            {
+                queue.Enqueue(confScenario);
+            }
+
+            foreach (var s in Shuffle(Scenarios))
+            {
+                queue.Enqueue(s);
+            }
+
+            return queue;
+        }
+
+        public string GetNextScenarioName()
+        {
+            return ScenarioQueue.Peek();
+        }
+
+        private string[] Shuffle(string[] array)
+        {
+            int n = array.Length;
+            var result = new string[n];
+            array.CopyTo(result, 0);
+            Random rnd = new();
+            
+            while (n > 1)
+            {
+                n--;
+                int k = rnd.Next(n + 1);
+                var value = result[k];
+                result[k] = result[n];
+                result[n] = value;
+            }
+
+            return result;
+        }
+
+
         public IScenario GetNextScenario()
         {
-            return new CodingScenario();
+            var scenario = ScenarioQueue.Dequeue();
+            ScenarioQueue.Enqueue(scenario);
+            return CreateInstance(scenario);
+        }
+
+        private IScenario CreateInstance(string name)
+        {
+            try
+            {
+                Type type = Type.GetType($"Coder.Scenarios.{name}");
+                object instance = Activator.CreateInstance(type);
+                return (IScenario)instance;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine($"Invalid scenario name: {name}");
+            }
+            return new StayOnlineScenario(); // в случае проблем возвращаем самый безопасный и ненавязчивый сценарий
         }
     }
 }
